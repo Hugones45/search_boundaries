22ed96e83a8858edb923b01605859c86d3db0967
Hugo <hugones45@gmail.com>
2025-06-19 23:49:34 -0300

    new back end

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 const allowedOrigins = [
     'http://127.0.0.1:5501',
     'http://localhost:5501',
-    'https://searchboundaries-production.up.railway.app'
+    'https://search-boundaries.onrender.com'
 ];
 
 // First CORS layer - middleware
